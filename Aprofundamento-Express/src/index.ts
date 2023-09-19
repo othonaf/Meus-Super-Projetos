b9f72b2cb123d64fb7fe22744d2c7d1b2c2dc2c6
Abraão Fontenele <othon.ab@hotmail.com>
2023-09-19 10:00:14 -0300

    Uploading updated files.

--- Aprofundamento-Express/src/index.ts
+++ Aprofundamento-Express/src/index.ts
@@ -4,22 +4,25 @@ import { Request, Response } from 'express';
 import { v4 as generateId } from 'uuid';
 import { todos } from "./data";
 import * as fs from 'fs';
-import { todo } from "node:test";
-import { error } from "node:console";
-import { forEachChild } from "typescript";
-
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-// EXERCÍCIO 01 and EXERCÍCIO 02 on the file "data.ts" 
+// EXERCISE 01 and EXERCISE 02 on the file "data.ts"
 
+/* EXERCISE 03
+        Objective: Build an endpoint that returns the entire to-do array:
+*/
 app.get('/todos', (req: Request, res: Response) => {
     res.status(201).send(todos)
 })
 
-// EXERCÍCIO 04
+/* EXERCISE 04
+        Objective: Build an endpoint that returns all tasks from the exercise 02 
+        according to a single status, that is, returns either completed 
+        tasks or only those that are still in progress:
+*/
 app.get('/tarefas/:status/:userId', (req: Request, res: Response) => {
     
         const taskStatus = req.params.status.toLowerCase()
@@ -42,7 +45,10 @@ app.get('/tarefas/:status/:userId', (req: Request, res: Response) => {
 
 })
 
-// EXERCÍCIO 05
+/* EXERCISE 05
+        Objective: Build a to-do creation endpoint. 
+        The response must return the updated to-do array.
+*/
 app.post('/create', (req: Request, res: Response) => {
     
     try {
@@ -55,7 +61,7 @@ app.post('/create', (req: Request, res: Response) => {
             userId: generateId(),
             todo: [
                 {
-                    id: todos.length +1,
+                    id: generateId(),
                     title: taskTitle,
                     completed: true
                 }
@@ -71,32 +77,36 @@ app.post('/create', (req: Request, res: Response) => {
     
 })
 
-// EXERCÍCIO 06
-app.post('/editar', (req: Request, res: Response) => {
+/* EXERCISE 06
+        Objective: Build an endpoint for editing the 
+        status of a to-do, that is, from complete to incomplete and vice versa.
+*/
+app.post('/editTask/:id', (req: Request, res: Response) => {
 
     try {
         const searchTask = req.body.title
-        const newState = req.body.completed
-        
-        // Searching a specific Task:
-        const taskUpdated:any = todos.find(
-            (user) => user.userId === searchTask)
-        
+        const Id = req.params.id
+        const user = todos.find((user) => user.userId === Id)
+          
         //Validating the Information.    
-        if(!searchTask)
-        throw new Error('This Task was not found or not existe!')   
+        if(!searchTask || !Id)
+        throw new Error('Please, type an Task and an Id!')  
     
-        // Changing the Task status:
-        taskUpdated.completed = !taskUpdated.completed
-        
-        res.status(201).send(taskUpdated)
+        // Searching a specific Task:
+        const updatedTask:any = user?.todo.find(task => task.title === searchTask)
+       // Changing the Task status:
+        updatedTask.completed = !updatedTask.completed 
+       
+       res.status(201).send(updatedTask)
 
     } catch (error: any) {
         res.end(error.message);
     }
 })
 
-// EXERCÍCIO 07
+/* EXERCISE 07
+        Objective: Build an endpoint that deletes a specific task according to its id.
+*/
 app.delete('/delete/:useId/:id', (req: Request, res: Response) => {
         try {
             const useId = req.params.useId
@@ -121,7 +131,9 @@ app.delete('/delete/:useId/:id', (req: Request, res: Response) => {
         }
 })
 
-// EXERCÍCIO 08
+/* EXERCISE 08 
+        Objective: Build an endpoint that returns all tasks for a given User Id.
+*/
 app.get('/selectById', (req: Request, res: Response) => {
     try {
         
@@ -137,7 +149,9 @@ app.get('/selectById', (req: Request, res: Response) => {
     }
 })
 
-// EXERCÍCIO 09
+/* EXERCISE 09
+        Objective: Build a to-do creation endpoint by User Id. 
+*/
 app.post('/createByUser/:id', (req: Request, res: Response) => {
     
     try {
@@ -158,7 +172,7 @@ app.post('/createByUser/:id', (req: Request, res: Response) => {
                     
         user.todo.push(newTask)
         
-        res.status(201).send("New task added suscesfully!")
+        res.status(201).send("New task added successfully!")
     
     } catch (error: any) {
         res.end(error.message);
@@ -166,14 +180,17 @@ app.post('/createByUser/:id', (req: Request, res: Response) => {
     
 })
 
-// EXERCÍCIO 10
-
+/* EXERCISE 10
+      Objective: At the endpoint of exercise 8, model the return for the 
+      following structure: an array with completed task objects and 
+      another object with tasks to do.
+*/
 app.get('/selectandall', (req: Request, res: Response) => {
     
         try {
             
             const Id = req.query.id
-            const selectedTask:any = todos.filter((todo) => todo.userId === Id)
+            const selectedTask = todos.filter((todo) => todo.userId === Id)
             const othersTask = todos.filter((todo) => todo.userId !== Id)
 
             const newTodos = todos.map((todo) => {
@@ -192,31 +209,33 @@ app.get('/selectandall', (req: Request, res: Response) => {
     
 })
 
-// EXERCÍCIO 11
-
+/* EXERCISE 11
+        Objective: Refactor the exercise 5 endpoint so that the added tasks 
+        are actually stored in the exercise 3 array.
+*/
 app.post('/editOnTheSource', (req: Request, res: Response) => {
     
     try {
         const taskTitle = req.body.title
         
         // Function to Add a new todo to the array "todos" 
-        function adicionarAfazer(novoAfazer:any) {
+        function addToDo(newToDo:any) {
             // Loading the File data.ts
             const dataFilePath = '/Users/Othon/OneDrive/Área de Trabalho/Backend/Meus-Super-Projetos/Aprofundamento-Express/src/data.ts';
             const data = require(dataFilePath);
         
             // Adding the new todo to the array "todos"
-            data.todos.push(novoAfazer);
+            data.todos.push(newToDo);
         
             // Write the updated file back to disk
             fs.writeFileSync(dataFilePath, `export const todos = ${JSON.stringify(data.todos, null, 2)};`, 'utf-8');
         }
         
-        const novoAfazer = {
+        const newToDo = {
             userId: generateId(),
             todo: [
               {
-                "id": todo.length +1,
+                "id": generateId(),
                 "title": taskTitle,
                 "completed": false
               },
@@ -224,8 +243,8 @@ app.post('/editOnTheSource', (req: Request, res: Response) => {
             ]
           };
   
-            adicionarAfazer(novoAfazer); 
-            res.status(201).send(novoAfazer) 
+            addToDo(newToDo); 
+            res.status(201).send(newToDo) 
            
     } catch (error) {
         res.status(404).end(error)
